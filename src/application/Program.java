7ceda6d3475fe17ca97812c734814bea92573f1d
Lucas Vale <lucas.vale.oli@gmail.com>
2019-01-03 03:34:36 -0200

    @override

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,34 +7,35 @@ import entities.SavingsAccount;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Account acc = new Account (1001, "Alex", 0.0);
-		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
-		
-		//UPCASTING
-		
-		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount (1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount (1004, "Ana", 0.0, 0.01);
-		
-		//DOWNCASTING
-		
-		BusinessAccount acc4 = (BusinessAccount)acc2;
-		acc4.loan(100.0);
-		
-		// BusimessAccount acc5 = (BusinessAccount)acc3;
-		if(acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.0);
-			System.out.println("Loan!");
-		}
-		
-		if(acc3 instanceof SavingsAccount){
-			SavingsAccount acc5 = (SavingsAccount)acc3;
-			acc5.updateBalance();
-			System.out.println("Update!");
-		}
-		
+
+		Account acc1 = new Account(1001, "Alex", 1000.0);
+		acc1.withdraw(200.0);
+		System.out.println(acc1.getBalance());
+
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
 	}
 
 }
+
+/*
+ * Account acc = new Account (1001, "Alex", 0.0); BusinessAccount bacc = new
+ * BusinessAccount(1002, "Maria", 0.0, 500.00);
+ * 
+ * //UPCASTING
+ * 
+ * Account acc1 = bacc; Account acc2 = new BusinessAccount (1003, "Bob", 0.0,
+ * 200.0); Account acc3 = new SavingsAccount (1004, "Ana", 0.0, 0.01);
+ * 
+ * //DOWNCASTING
+ * 
+ * BusinessAccount acc4 = (BusinessAccount)acc2; acc4.loan(100.0);
+ * 
+ * // BusimessAccount acc5 = (BusinessAccount)acc3; if(acc3 instanceof
+ * BusinessAccount) { BusinessAccount acc5 = (BusinessAccount)acc3;
+ * acc5.loan(200.0); System.out.println("Loan!"); }
+ * 
+ * if(acc3 instanceof SavingsAccount){ SavingsAccount acc5 =
+ * (SavingsAccount)acc3; acc5.updateBalance(); System.out.println("Update!"); }
+ */
